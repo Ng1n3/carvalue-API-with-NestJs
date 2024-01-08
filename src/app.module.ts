2f0c900f0cbbee2926211c48ca75aabc927fd3f3
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-08 09:45:22 +0100

    basic post request to test sqlite config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true,
-  })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,11 @@
+import {IsEmail, IsString, Length} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+
+  @IsString()
+  @Length(5)
+  password: string;
+}
