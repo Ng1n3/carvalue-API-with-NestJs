597f7bd9ad810d2a00bda2ab167a218cea5187f0
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-13 21:28:34 +0100

    signup tests returning user via cookies

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,15 +16,33 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'newg@gmail.com'
+    const email = 'newg@gmail.com';
     return request(app.getHttpServer())
-    .post('/auth/signup')
-    .send({ email, password: 'mypassword'})
-    .expect(201)
-    .then((res) => {
-      const {id, email} = res.body
-      expect(id).toBeDefined();
-      expect(email).toEqual(email)
-    })
-  })
+      .post('/auth/signup')
+      .send({ email, password: 'mypassword' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual(email);
+      });
+  });
+
+  it('signup as new user then get the currently logged in user', async () => {
+    const email = 'asfnew@gmail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'mypassword' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
+  });
 });
