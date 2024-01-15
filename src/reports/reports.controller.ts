eb74a2c0c51dd8d04fdff81361edd3f291ff5c02
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-15 09:51:33 +0100

    implement estimate logic

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -17,6 +17,7 @@ import { ReportDto } from './dtos/reports.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { getEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -33,5 +34,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: getEstimateDto) {
+    return this.reportsService.createEstimate(query);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,9 +4,26 @@ import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/createReportDto';
 import { User } from 'src/users/user.entity';
+import { getEstimateDto } from './dtos/get-estimate.dto';
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createEstimate({ make, model, lng, lat, year, mileage }: getEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', {lng})
+      .andWhere('lat - :lat BETWEEN -5 AND 5', {lat})
+      .andWhere('year - :year BETWEEN -3 AND 3', {year})
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({mileage})
+      .limit(3)
+      .getRawOne();
+  }
 
   create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
@@ -15,8 +32,8 @@ export class ReportsService {
   }
 
   async changeApproval(id: string, approved: boolean) {
-    const report = await this.repo.findOne({where: {id: parseInt(id)}});
-    if(!report) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
       throw new NotFoundException('Report not found');
     }
 
